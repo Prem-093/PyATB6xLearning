02729c0013d9f66c925f1369b5011e89bc5af8fe
Prem-093 <tripathiprem093@gmail.com>
2025-10-09 11:34:13 +0530

    new files edit

---
+++ src/ex_01_Python_Basics/Lab003_print.py
@@ -0,0 +1,13 @@
+## print(self, *args, sep=' ',end='\n',file='none')
+
+# self - please ignore  (oops)
+# *args - unlimited number of comma seperated arguments.
+
+print("Prem Tripathi",123,"Manu","Sonu", sep=',',end='----')
+print("Kartikey",12,"Manu","Nidhi", sep=',')
+#Print("hi")//Case sensitive
+   # Print("hi")
+   # ^^^^^
+#NameError: name 'Print' is not defined. Did you mean: 'print'?
+
+#Process finished with exit code 1
